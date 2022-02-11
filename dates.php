ff5d0329e737c57e38fcfdcab621e77431c7653f
virgo21510023 <virgo_21510023@stimata.ac.id>
2022-02-11 22:06:33 +0700

    task7

--- dates.php
+++ dates.php
@@ -1,3 +1,20 @@
 <?php
-	echo date("l", strtotime("2 September 2030"));
+	function salam($waktu, $nama){
+		return "selamat $waktu, $nama!";
+	}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>latihan function</title>
+</head>
+<body>
+	<h1>
+		<?=
+		salam("malam", "virgo")
+		?>
+	</h1>
+</body>
+</html>
